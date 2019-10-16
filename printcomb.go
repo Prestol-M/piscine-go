a30fc2e54aecd587b0d43621843388f673fa2687
Prestol-M <mukhamed.sattarov@mail.ru>
2019-10-16 22:38:17 +0600

    printcomb2.go

--- printcomb.go
+++ printcomb.go
@@ -2,52 +2,30 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func IntToRune(num int) (runenum rune) {
-	switch num {
-	case 0:
-		runenum = '0'
-	case 1:
-		runenum = '1'
-	case 2:
-		runenum = '2'
-	case 3:
-		runenum = '3'
-	case 4:
-		runenum = '4'
-	case 5:
-		runenum = '5'
-	case 6:
-		runenum = '6'
-	case 7:
-		runenum = '7'
-	case 8:
-		runenum = '8'
-	case 9:
-		runenum = '9'
-	}
-	return runenum
-}
-
 func PrintComb() {
+	a := '0'
+	b := '0'
+	c := '0'
 
-	for i := 0; i < 10; i++ {
-		for j := i + 1; j < 10; j++ {
-			for k := j + 1; k < 10; k++ {
-				if i == 7 && j == 8 && k == 9 {
-					z01.PrintRune(IntToRune(i))
-					z01.PrintRune(IntToRune(j))
-					z01.PrintRune(IntToRune(k))
-					z01.PrintRune('\n')
-					break
-				} else {
-					z01.PrintRune(IntToRune(i))
-					z01.PrintRune(IntToRune(j))
-					z01.PrintRune(IntToRune(k))
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+	for a <= '9' {
+		for b <= '9' {
+			for c <= '9' {
+				if a < b && b < c {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(c)
+					if a != '7' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
 				}
+				c++
 			}
+			c = '0'
+			b++
 		}
+		b = '0'
+		a++
 	}
-
+	z01.PrintRune('\n')
 }
